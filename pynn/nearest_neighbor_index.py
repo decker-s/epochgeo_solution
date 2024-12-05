a758169f3c3ea92f391c67cdc4a19770f1345185
decker-s <jacobsoutherland97@gmail.com>
2024-12-05 02:51:55 +0000

    initial kd tree implementation also includes a wider range of tests

--- pynn/nearest_neighbor_index.py
+++ pynn/nearest_neighbor_index.py
@@ -1,20 +1,62 @@
 import math
 
-
 class NearestNeighborIndex:
-    """
-    TODO give me a decent comment
 
-    NearestNeighborIndex is intended to index a set of provided points to provide fast nearest
-    neighbor lookup. For now, it is simply a stub that performs an inefficient traversal of all
-    points every time.
-    """
+    class KDNode:
+        def __init__(self, point, left=None, right=None):
+            self.point = point
+            self.left = left
+            self.right = right
 
     def __init__(self, points):
-        """
-        takes an array of 2d tuples as input points to be indexed.
-        """
         self.points = points
+        self.root = self.build_kd_tree(points, depth=0)
+
+    def build_kd_tree(self, points, depth=0):
+        if not points:
+            return None
+
+        axis = depth % 2
+        points.sort(key=lambda x: x[axis])
+        median = len(points) // 2
+
+        return self.KDNode(
+            point=points[median],
+            left=self.build_kd_tree(points[:median], depth + 1),
+            right=self.build_kd_tree(points[median + 1:], depth + 1)
+        )
+
+    def _nearest(self, node, query_point, depth, best):
+        if node is None:
+            return best
+
+        dist = self._distance(query_point, node.point)
+        if dist < best[1]:
+            best = (node.point, dist)
+
+        axis = depth % 2
+        diff = query_point[axis] - node.point[axis]
+        near_side = node.left if diff < 0 else node.right
+        far_side = node.right if diff < 0 else node.left
+
+        best = self._nearest(near_side, query_point, depth + 1, best)
+
+        if abs(diff) < best[1]:
+            best = self._nearest(far_side, query_point, depth + 1, best)
+
+        return best
+
+    @staticmethod
+    def _distance(point1, point2):
+        deltax = point1[0] - point2[0]
+        deltay = point1[1] - point2[1]
+        return math.sqrt(deltax * deltax + deltay * deltay)
+
+
+    def find_nearest_fast(self, query_point):
+        if not self.root:
+            return None
+        return self._nearest(self.root, query_point, depth=0, best=(None, float('inf')))[0]
 
     @staticmethod
     def find_nearest_slow(query_point, haystack):
@@ -36,27 +78,6 @@ class NearestNeighborIndex:
 
         return min_point
 
-    def find_nearest_fast(self, query_point):
-        """
-        TODO: Re-implement me with your faster solution.
-
-        find_nearest_fast returns the point that is closest to query_point. If there are no indexed
-        points, None is returned.
-        """
-
-        min_dist = None
-        min_point = None
-
-        for point in self.points:
-            deltax = point[0] - query_point[0]
-            deltay = point[1] - query_point[1]
-            dist = math.sqrt(deltax * deltax + deltay * deltay)
-            if min_dist is None or dist < min_dist:
-                min_dist = dist
-                min_point = point
-
-        return min_point
-
     def find_nearest(self, query_point):
         """
         TODO comment me.
@@ -64,3 +85,69 @@ class NearestNeighborIndex:
 
         # TODO implement me so this class runs much faster.
         return self.find_nearest_fast(query_point)
+
+
+#
+# class NearestNeighborIndex:
+#     """
+#     TODO give me a decent comment
+#
+#     NearestNeighborIndex is intended to index a set of provided points to provide fast nearest
+#     neighbor lookup. For now, it is simply a stub that performs an inefficient traversal of all
+#     points every time.
+#     """
+#
+#     def __init__(self, points):
+#         """
+#         takes an array of 2d tuples as input points to be indexed.
+#         """
+#         self.points = points
+#
+#     @staticmethod
+#     def find_nearest_slow(query_point, haystack):
+#         """
+#         find_nearest_slow returns the point that is closest to query_point. If there are no indexed
+#         points, None is returned.
+#         """
+#
+#         min_dist = None
+#         min_point = None
+#
+#         for point in haystack:
+#             deltax = point[0] - query_point[0]
+#             deltay = point[1] - query_point[1]
+#             dist = math.sqrt(deltax * deltax + deltay * deltay)
+#             if min_dist is None or dist < min_dist:
+#                 min_dist = dist
+#                 min_point = point
+#
+#         return min_point
+#
+#     def find_nearest_fast(self, query_point):
+#         """
+#         TODO: Re-implement me with your faster solution.
+#
+#         find_nearest_fast returns the point that is closest to query_point. If there are no indexed
+#         points, None is returned.
+#         """
+#
+#         min_dist = None
+#         min_point = None
+#
+#         for point in self.points:
+#             deltax = point[0] - query_point[0]
+#             deltay = point[1] - query_point[1]
+#             dist = math.sqrt(deltax * deltax + deltay * deltay)
+#             if min_dist is None or dist < min_dist:
+#                 min_dist = dist
+#                 min_point = point
+#
+#         return min_point
+#
+#     def find_nearest(self, query_point):
+#         """
+#         TODO comment me.
+#         """
+#
+#         # TODO implement me so this class runs much faster.
+#         return self.find_nearest_fast(query_point)
--- tests/test_nearest_neighbor_index.py
+++ tests/test_nearest_neighbor_index.py
@@ -8,6 +8,44 @@ from pynn import NearestNeighborIndex
 
 
 class NearestNeighborIndexTest(unittest.TestCase):
+    def test_empty(self):
+
+        # input an empty dataset to ensure the algorithm returns none
+        uut = NearestNeighborIndex([])
+        self.assertIsNone(uut.find_nearest((0, 0)))
+
+    def test_singe_point(self):
+
+        # input a single point and ensure the algorithm returns that point
+        uut = NearestNeighborIndex([(1, 1)])
+        self.assertEqual((1, 1), uut.find_nearest((1, 1)))
+        self.assertEqual((1, 1), uut.find_nearest((6, 6)))
+        self.assertEqual((1, 1), uut.find_nearest((12, 12)))
+
+    def test_identical(self):
+
+        # test to make sure identical points function correctly
+        uut = NearestNeighborIndex([(3, 3), (3, 3), (3, 3)])
+        self.assertEqual((3, 3), uut.find_nearest((1, 1)))
+
+    def test_large(self):
+
+        # test performance and integrity with large dataset
+
+        def rand_point():
+            return (random.uniform(-1e7, 1e7), random.uniform(-1e7, 1e7))
+
+        index_points = [rand_point() for _ in range (100000)]
+        query_points = [rand_point() for _ in range (1000)]
+
+        uut = NearestNeighborIndex(index_points)
+
+        for query_point in query_points:
+            nearest = uut.find_nearest(query_point)
+            self.assertIsNotNone(nearest)
+
+
+
     def test_basic(self):
         """
         test_basic tests a handful of nearest neighbor queries to make sure they return the right
@@ -28,6 +66,38 @@ class NearestNeighborIndexTest(unittest.TestCase):
         self.assertEqual((1, 0), uut.find_nearest((0, 0)))
         self.assertEqual((-1000, 20), uut.find_nearest((-2000, 0)))
         self.assertEqual((42, 3.14159), uut.find_nearest((40, 3)))
+
+
+    def test_dynamic_updates(self):
+
+        # test to ensure the algorithm works even with insertions and deletions
+        points = [(0, 0), (12, 12), (23, 23)]
+        uut = NearestNeighborIndex(points)
+
+        # do a query before an update
+        self.assertEqual((12, 12), uut.find_nearest((13, 14)))
+
+        # update by rebuilding the tree
+        updates = points + [(13, 13)]
+        uut = NearestNeighborIndex(updates)
+        self.assertEqual((13, 13), uut.find_nearest((13, 14)))
+
+        # update by removing a point
+        updated_list = [(12, 12), (23, 23)]
+        uut = NearestNeighborIndex(updated_list)
+        self.assertEqual((23, 23), uut.find_nearest((19, 19)))
+
+
+    def test_boundary(self):
+
+        # test with extremely (relatively) small/large numbers
+        points = [(1e10, 1e10), (-1e10, -1e10), (0,0)]
+        uut = NearestNeighborIndex(points)
+
+        self.assertEqual((1e10, 1e10), uut.find_nearest((1e10 - 1, 1e10 - 1)))
+        self.assertEqual((-1e10, -1e10), uut.find_nearest((-1e10 + 1, -1e10 + 1)))
+        self.assertEqual((0, 0), uut.find_nearest((3, 3)))
+
 
     def test_benchmark(self):
         """
@@ -62,5 +132,3 @@ class NearestNeighborIndexTest(unittest.TestCase):
         print(f"slow time: {slow_time:0.2f}sec")
         print(f"new time: {new_time:0.2f}sec")
         print(f"speedup: {(slow_time / new_time):0.2f}x")
-
-    # TODO: Add more test cases to ensure your index works in different scenarios
