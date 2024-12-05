cbdbb6923053770e27117d4d7bbd04558b4520d7
decker-s <jacobsoutherland97@gmail.com>
2024-12-05 03:10:27 +0000

    more comments in the actual implementation of the algorithm

--- pynn/nearest_neighbor_index.py
+++ pynn/nearest_neighbor_index.py
@@ -1,7 +1,6 @@
 import math
 
 class NearestNeighborIndex:
-
     class KDNode:
         def __init__(self, point, left=None, right=None):
             self.point = point
@@ -13,13 +12,36 @@ class NearestNeighborIndex:
         self.root = self.build_kd_tree(points, depth=0)
 
     def build_kd_tree(self, points, depth=0):
+        """
+        A k-d tree uses recursion to build a tree from a list of points.
+        It alternates splitting space along each dimension (in this case x and y).
+        The points themselves are divided into two subsets at each level based on median values of the splitting
+        dimension.
+
+
+        Arguments are:
+            - points: list of tuples (2D points to put into the k-d tree)
+            - depth: int (current depth of recursion to determine the splitting dimension.
+
+        Returns the root of the tree
+        """
+
+        # edge case if there are no points, just return none.
+        # If you've reached the end of a branch you will end up here.
         if not points:
             return None
 
+        # determines axis to split along depending on depth of recursion (alternates splitting x and y)
         axis = depth % 2
+
+        # sorts points based off of the current axis, depth 0 is x, depth 1 is y, depth 2 is x, depth 3 is y, etc..
         points.sort(key=lambda x: x[axis])
+
+        # find median point after sorting which splits data into two (rougly) equal (sub)sets
         median = len(points) // 2
 
+        # creation of a new node. the median now becomes the root at this level of the tree.
+        # the left child is made from points to left of median, right child from points to right of median
         return self.KDNode(
             point=points[median],
             left=self.build_kd_tree(points[:median], depth + 1),
@@ -27,20 +49,43 @@ class NearestNeighborIndex:
         )
 
     def _nearest(self, node, query_point, depth, best):
+        """
+        recursively search the tree for the nearest point.
+
+        Arguments are:
+            - node: KDNode (the current node)
+            - query_point: tuple (the point we are comparing other points to, we want to find what is closest to this)
+            - depth: int (current level of recursion)
+            - best: tuple (current best candidate (point, distance))
+
+        Returns the best candidate after searching current node and corresponding subtrees.
+
+        """
+
+        # node empty? just return the previous best
         if node is None:
             return best
 
+        # compute distance using a helper function
         dist = self._distance(query_point, node.point)
+
+        # if distance beats our previous best, set that as our new best
         if dist < best[1]:
             best = (node.point, dist)
 
+        # determine axis for our current depth
         axis = depth % 2
+
+        # calculate difference to check which subtree to explore first
         diff = query_point[axis] - node.point[axis]
         near_side = node.left if diff < 0 else node.right
         far_side = node.right if diff < 0 else node.left
 
+        # begin recursive search on the near side first
         best = self._nearest(near_side, query_point, depth + 1, best)
 
+        # possible the far side could contain a closer point so we check it.
+        # this case can occur if distance to splitting plane is < current best
         if abs(diff) < best[1]:
             best = self._nearest(far_side, query_point, depth + 1, best)
 
@@ -48,6 +93,7 @@ class NearestNeighborIndex:
 
     @staticmethod
     def _distance(point1, point2):
+        # normal euclidean distance function.
         deltax = point1[0] - point2[0]
         deltay = point1[1] - point2[1]
         return math.sqrt(deltax * deltax + deltay * deltay)
@@ -56,6 +102,9 @@ class NearestNeighborIndex:
     def find_nearest_fast(self, query_point):
         if not self.root:
             return None
+
+        # we set best to a tuple of None (because initially no point is found) and inf because any valid distance will
+        # be smaller. [0] contains the point itself, [1] being the distance
         return self._nearest(self.root, query_point, depth=0, best=(None, float('inf')))[0]
 
     @staticmethod
@@ -79,11 +128,6 @@ class NearestNeighborIndex:
         return min_point
 
     def find_nearest(self, query_point):
-        """
-        TODO comment me.
-        """
-
-        # TODO implement me so this class runs much faster.
         return self.find_nearest_fast(query_point)
 
 
